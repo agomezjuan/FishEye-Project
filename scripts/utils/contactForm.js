5740dceca495696cf660f58eb415a1c026822c0e
Alberto <agomezjuan@hotmail.com>
2022-04-10 11:51:51 +0200

    added validation to contact form

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,8 +7,6 @@ async function displayModal() {
     }
   });
 
-  console.log(photographer);
-
   const modal = document.getElementById("contact_modal");
   modal.style.display = "flex";
   modal.style.position = "fixed";
@@ -25,35 +23,56 @@ async function displayModal() {
   prenomLabel.innerText = "Prénom";
   const inputPrenom = document.createElement("input");
   inputPrenom.setAttribute("type", "text");
+  inputPrenom.setAttribute("id", "prenom");
+  const errorPrenom = document.createElement("span");
+  errorPrenom.setAttribute("id", "prenom_error");
+  errorPrenom.style.color = "red";
   prenomDiv.appendChild(prenomLabel);
   prenomDiv.appendChild(inputPrenom);
+  prenomDiv.appendChild(errorPrenom);
 
   const nomDiv = document.createElement("div");
   const nomLabel = document.createElement("label");
   nomLabel.innerText = "Nom";
   const inputNom = document.createElement("input");
   inputNom.setAttribute("type", "text");
+  inputNom.setAttribute("id", "nom");
+  const errorNom = document.createElement("span");
+  errorNom.setAttribute("id", "nom_error");
+  errorNom.style.color = "red";
   nomDiv.appendChild(nomLabel);
   nomDiv.appendChild(inputNom);
+  nomDiv.appendChild(errorNom);
 
   const emailDiv = document.createElement("div");
   const emailLabel = document.createElement("label");
   emailLabel.innerText = "Email";
   const inputEmail = document.createElement("input");
   inputEmail.setAttribute("type", "email");
+  inputEmail.setAttribute("id", "email");
+  const errorEmail = document.createElement("span");
+  errorEmail.setAttribute("id", "email_error");
+  errorEmail.style.color = "red";
   emailDiv.appendChild(emailLabel);
   emailDiv.appendChild(inputEmail);
+  emailDiv.appendChild(errorEmail);
 
   const textDiv = document.createElement("div");
   const textLabel = document.createElement("label");
   textLabel.innerText = "Message";
   const textarea = document.createElement("textarea");
   textarea.setAttribute("rows", "5");
+  textarea.setAttribute("id", "message");
+  const errorMessage = document.createElement("span");
+  errorMessage.setAttribute("id", "message_error");
+  errorMessage.style.color = "red";
   textDiv.appendChild(textLabel);
   textDiv.appendChild(textarea);
+  textDiv.appendChild(errorMessage);
 
   const button = document.createElement("button");
   button.setAttribute("class", "contact_button");
+  button.setAttribute("type", "submit");
   button.innerText = "Envoyer";
 
   form.appendChild(prenomDiv);
@@ -61,6 +80,18 @@ async function displayModal() {
   form.appendChild(emailDiv);
   form.appendChild(textDiv);
   form.appendChild(button);
+
+  //create event to validate form fields
+  const sendButton = document.querySelector("#contact-form .contact_button");
+
+  sendButton.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      alert("Message envoyé avec succès.");
+      closeModal();
+    }
+  });
 }
 
 function closeModal() {
@@ -73,40 +104,29 @@ function validate() {
   var regex1 = /^[A-Za-z\s]+$/;
   var regex2 = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
 
-  const date = new Date(birthVerit.value);
+  const prenom = document.getElementById("prenom");
+  const nom = document.getElementById("nom");
+  const emailVerif = document.getElementById("email");
+  const message = document.getElementById("message");
 
-  const timeStampDate = Date.parse(date);
-
-  const actualDate = Date.now();
-
+  // check message
   if (
-    prenom.value === "" ||
-    prenom.value.length < 2 ||
-    !prenom.value.match(regex1)
+    message.value === "" ||
+    message.value.length < 12 ||
+    !message.value.match(regex1)
   ) {
-    document.getElementById("prenom_error").innerHTML =
-      "Veuillez entrer au moins 2 caractères valables.";
-    prenom.focus();
-    prenom.style.border = "2px solid #fe142f";
+    document.getElementById("message_error").innerHTML =
+      "Veuillez entrer au moins 12 caractères valables.";
+    message.focus();
+    message.style.border = "2px solid #fe142f";
 
     errorVerif = true;
   } else {
-    document.getElementById("prenom_error").innerHTML = "";
-    prenom.style.border = "0px solid red";
+    document.getElementById("message_error").innerHTML = "";
+    message.style.border = "0px solid red";
   }
 
-  if (nom.value === "" || nom.value.length < 2 || !nom.value.match(regex1)) {
-    document.getElementById("nom_error").innerHTML =
-      "Veuillez entrer au moins 2 caractères valables.";
-    nom.focus();
-    nom.style.border = "2px solid #fe142f";
-
-    errorVerif = true;
-  } else {
-    document.getElementById("nom_error").innerHTML = "";
-    nom.style.border = "0px solid red";
-  }
-
+  // check email
   if (
     emailVerif.value == null ||
     emailVerif.value == "" ||
@@ -123,53 +143,37 @@ function validate() {
     emailVerif.style.border = "0px solid red";
   }
 
-  if (birthVerit.value == "") {
-    document.getElementById("birrth_error").innerHTML =
-      "Veuillez entrer votre date de naissance.";
-    birthVerit.focus();
-    birthVerit.style.border = "2px solid #fe142f";
-
-    errorVerif = true;
-  } else if (timeStampDate > actualDate) {
-    document.getElementById("birrth_error").innerHTML =
-      "Votre date de naissance ne peut pas être postérieure à la date actuelle.";
-    birthVerit.focus();
-    birthVerit.style.border = "2px solid #fe142f";
-  } else {
-    document.getElementById("birrth_error").innerHTML = "";
-    birthVerit.style.border = "0px solid red";
-  }
-
-  if (tournoisVerif.value == null || tournoisVerif.value == "") {
-    document.getElementById("tournois_error").innerHTML =
-      "Vous devez choisir une option.";
-    tournoisVerif.focus();
-    tournoisVerif.style.border = "2px solid #fe142f";
+  // check nom
+  if (nom.value === "" || nom.value.length < 2 || !nom.value.match(regex1)) {
+    document.getElementById("nom_error").innerHTML =
+      "Veuillez entrer au moins 2 caractères valables.";
+    nom.focus();
+    nom.style.border = "2px solid #fe142f";
 
     errorVerif = true;
   } else {
-    document.getElementById("tournois_error").innerHTML = "";
-    tournoisVerif.style.border = "0px solid red";
+    document.getElementById("nom_error").innerHTML = "";
+    nom.style.border = "0px solid red";
   }
 
-  if (!document.querySelector('input[name = "location"]:checked')) {
-    document.getElementById("city_error").innerHTML =
-      "Vous devez choisir une option.";
+  // check prenom
+  if (
+    prenom.value === "" ||
+    prenom.value.length < 2 ||
+    !prenom.value.match(regex1)
+  ) {
+    document.getElementById("prenom_error").innerHTML =
+      "Veuillez entrer au moins 2 caractères valables.";
+    prenom.focus();
+    prenom.style.border = "2px solid #fe142f";
 
     errorVerif = true;
   } else {
-    document.getElementById("city_error").innerHTML = "";
+    document.getElementById("prenom_error").innerHTML = "";
+    prenom.style.border = "0px solid red";
   }
 
-  if (!conditionVerif.checked) {
-    document.getElementById("condition_error").innerHTML =
-      "Vous devez vérifier que vous acceptez les termes et conditions";
-
-    errorVerif = true;
-  } else {
-    document.getElementById("condition_error").innerHTML = "";
-  }
-
+  // if any error return true
   if (errorVerif === true) {
     return false;
   } else {
